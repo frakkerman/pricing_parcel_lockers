4a473c9ae8923a601e0360d75ef675cfb264c9f8
Akkerman <f.r.akkerman@utwente.nl>
2023-06-14 21:55:34 +0200

    different CNN arch

--- Src/Utils/Predictors.py
+++ Src/Utils/Predictors.py
@@ -7,41 +7,46 @@ class CNN_2d(nn.Module):
     def __init__(self,dim,n_layers):
         super().__init__()
         
-        kernel1 = (3,3)
-        kernel2 = (2,2)
-        dilation=0
-        stride=(2,2)
-        padding=1
-        out_channels=20
+        kernel1 = (3,3)#conv
+        kernel2 = (2,2)#pool
+        dilation=1
+        stride=(1,1)
+        padding=(1,1)
         
-        h1 = np.floor((dim+2*padding-dilation*(kernel1[0]-1)-1)/stride[0]+1)
-        w1 = np.floor((dim+2*padding-dilation*(kernel1[1]-1)-1)/stride[1]+1)
-        h2 = np.floor((h1+2*padding-kernel2[0])/stride[0]+1)
-        w2 = np.floor((w1+2*padding-kernel2[1])/stride[0]+1)
+        out_channels=16#tune
+        dropout = 0.5#tune
+        #learning rate, batch size(8,16,32,)
         
-        self.conv1 = nn.Conv2d(in_channels=n_layers, out_channels=out_channels,kernel_size=kernel1,padding=padding)
-        self.relu1 = nn.ReLU()
+        h1 = np.floor(((dim+2*padding[0]-dilation*(kernel1[0]-1)-1)/stride[0])+1)
+        w1 = np.floor(((dim+2*padding[1]-dilation*(kernel1[1]-1)-1)/stride[1])+1)
+        h2 = np.floor(((h1+2*padding[0]-dilation*(kernel1[0]-1)-1)/stride[0])+1)
+        w2 = np.floor(((w1+2*padding[1]-dilation*(kernel1[1]-1)-1)/stride[1])+1)
+        h3 = np.floor(((h2+2*padding[0]-kernel2[0])/stride[0])+1)
+        w3 = np.floor(((w2+2*padding[1]-kernel2[1])/stride[0])+1)
+        
+        self.conv1 = nn.Conv2d(in_channels=n_layers,dilation=dilation, 
+                               out_channels=out_channels,kernel_size=kernel1,padding=padding)
+        self.conv2 = nn.Conv2d(in_channels=out_channels,dilation=dilation, 
+                               out_channels=2*out_channels,kernel_size=kernel1,padding=padding)
         self.avgpool1 = nn.AvgPool2d(kernel_size=kernel2, stride=stride,padding=padding)
         
-        self.fc1 = nn.Linear(in_features=int(out_channels*h2*w2), out_features=40)
-        self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(40, 20)
-        self.relu4 = nn.ReLU()
-        self.fc3 = nn.Linear(20, 1)
+        self.fc1 = nn.Linear(in_features=int(2*out_channels*h3*w3), out_features=256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(64, 1)
+        
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
 		# pass the input through our first set of CONV => RELU =>
 		# POOL layers
-        x = self.conv1(x)
-        x = self.relu1(x)
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
         x = self.avgpool1(x)
 		# flatten the output from the previous layer and pass it
 		# through our only set of FC => RELU layers
         x = flatten(x)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        x = self.fc2(x)
-        x = self.relu4(x)
+        x = self.dropout(nn.functional.relu(self.fc1(x)))
+        x = self.dropout(nn.functional.relu(self.fc2(x)))
         output = self.fc3(x)
 
         return output
@@ -63,7 +68,7 @@ class CNN_3d(nn.Module):
         
         self.conv1 = nn.Conv3d(in_channels=n_layers, out_channels=20,kernel_size=(3, 3, 3),padding=(1,1,1))
         self.relu1 = nn.ReLU()
-        self.avgpool1 = nn.AvgPool2d(kernel_size=(3, 2, 2), stride=(2, 2))
+        self.avgpool1 = nn.AvgPool3d(kernel_size=(3, 2, 2), stride=(2, 2))
         
         self.fc1 = nn.Linear(in_features=80, out_features=40)
         self.relu3 = nn.ReLU()
