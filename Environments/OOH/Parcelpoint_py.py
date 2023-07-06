cleanup, fix mistake in costs calculation

Environments/OOH/Parcelpoint_py.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import numpy as np
 import numpy.ma as ma
-from Src.Utils.Utils import Space,get_dist_mat_HGS,get_fleet
+from Src.Utils.Utils import get_dist_mat_HGS,get_fleet
 from Environments.OOH.containers import Location,ParcelPoint,ParcelPoints,Vehicle,Fleet,Customer
 from Environments.OOH.env_utils import utils_env
 from Environments.OOH.customerchoice import customerchoicemodel
@@ -52,7 +52,6 @@ class Parcelpoint_py(object):
             self.utils = utils_env(Location,Vehicle,Fleet,ParcelPoint,ParcelPoints,self.veh_capacity,self.n_vehicles,self.pp_capacity,self.data,self.dist_matrix,hgs_time)
             self.depot = self.coords[0]
             self.parcelPoints = self.utils.get_parcelpoints_from_data(self.coords[-self.n_parcelpoints:],self.n_unique_customer_locs)
-            self.action_space = Space(size=2**20)#we only select from the 20 closest parcelpoints
             self.get_customer = self.get_new_customer_from_data
             self.num_cust_loc = len(self.dist_matrix)-len(self.parcelPoints["parcelpoints"])-1
             self.dist_scaler = np.amax(self.dist_matrix)
@@ -60,7 +59,6 @@ class Parcelpoint_py(object):
             self.depot = Location(50,50,0,0)
             self.utils = utils_env(Location,Vehicle,Fleet,ParcelPoint,ParcelPoints,self.veh_capacity,self.n_vehicles,self.pp_capacity,self.data,self.dist_matrix,hgs_time)
             self.parcelPoints = self.utils.get_parcelpoints()
-            self.action_space = Space(size=2**self.n_parcelpoints)
             self.get_customer = self.generate_new_customer
             self.dist_scaler = 10
                 

Src/Utils/Utils.py
@@ -58,12 +58,12 @@ def save_plots_test_runs(test_returns,test_std,step_time,config):
     np.save(config.paths['results'] + "eval_step_time", step_time)
     x = config.save_after * np.arange(0, len(test_returns))
     plt.figure()
-    plt.ylabel("Total HGS distance")
+    plt.ylabel("Total HGS time")
     plt.xlabel("Episode")
     plt.title("Performance")
     plt.plot(x,test_returns,color='#CC4F1B')
     plt.fill_between(x,np.array(test_returns)+np.array(test_std),np.array(test_returns)-np.array(test_std),alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
-    plt.savefig(config.paths['results'] + "dist_eval_runs.png")
+    plt.savefig(config.paths['results'] + "hgs_time_eval_runs.png")
     plt.close()
     
     plt.figure()
@@ -125,10 +125,10 @@ def save_plots_stats(stats,costs,run_time,actions,config,episode):
     #4 boxplot final hgs distance
     costs =  np.array([i for i in costs if i != 0])
     plt.figure()
-    plt.ylabel("Total distance")
+    plt.ylabel("Total time")
     plt.title("Performance")
     plt.boxplot(costs)
-    plt.savefig(config.paths['results'] + "box_hgs_distance.png")
+    plt.savefig(config.paths['results'] + "box_hgs_time.png")
     plt.close()
     
     #5 count number of unique parcelpoints
@@ -137,7 +137,7 @@ def save_plots_stats(stats,costs,run_time,actions,config,episode):
     
     #6 bar times
     added_costs_home = (config.del_time/60)
-    drive_time = np.mean(costs) / config.truck_speed
+    drive_time = np.mean(costs)
     service_time = added_costs_home*count_home
     plt.figure()
     plt.ylabel("Hours")
@@ -148,9 +148,10 @@ def save_plots_stats(stats,costs,run_time,actions,config,episode):
     plt.close()
     
     #7cost and revenue
-    cost_multiplier = (config.driver_wage+config.fuel_cost*config.truck_speed) / config.truck_speed
+    cost_multiplier = (config.driver_wage+config.fuel_cost*config.truck_speed) / 3600
     revenue = (len(info)/(episode+1))*config.revenue
     total_costs = added_costs_home*count_home+(np.mean(costs)*cost_multiplier)
+    total_costs += count_home*config.home_failure*config.failure_cost#costs of failed delivery
     plt.figure()
     plt.ylabel("Monetary unit")
     plt.title("Performance")
@@ -167,21 +168,8 @@ def save_plots_stats(stats,costs,run_time,actions,config,episode):
     np.save(config.paths['results'] + "pricing_revenue", pricing_revenue)
     np.save(config.paths['results'] + "revenues", revenue)
     np.save(config.paths['results'] + "total_costs", total_costs)
-    np.save(config.paths['results'] + "hgs_distances", costs)
+    np.save(config.paths['results'] + "hgs_times", costs)
     np.save(config.paths['results'] + "percent_home_deliveries", count_home/len(info))
-    
-
-
-class Space:
-    def __init__(self, low=[0], high=[1], dtype=np.uint8, size=-1):
-        if size == -1:
-            self.shape = np.shape(low)
-        else:
-            self.shape = (size, )
-        self.low = np.array(low)
-        self.high = np.array(high)
-        self.dtype = dtype
-        self.n = len(self.low)
 
 
 def save_training_checkpoint(state, is_best, episode_count):

Src/config.py
@@ -60,7 +60,7 @@ class Config(object):
             self.coords,self.dist_matrix,self.n_parcelpoints,self.adjacency = [],[],6,np.ones(6)
 
         # Get the domain and algorithm
-        self.env, self.gym_env, self.cont_actions = self.get_domain(args.env_name, args=args, debug=args.debug,
+        self.env, self.gym_env = self.get_domain(args.env_name, args=args, debug=args.debug,
                                                                path=path.join(self.paths['root'], 'Environments'))
         self.env.seed(seed)
 
@@ -102,7 +102,7 @@ class Config(object):
                       veh_capacity=args.veh_capacity,parcelpoint_capacity=args.parcelpoint_capacity,incentive_sens=args.incentive_sens,base_util=args.base_util,
                       home_util=args.home_util,reopt=args.reopt,load_data=args.load_data,coords=self.coords,dist_matrix=self.dist_matrix,
                       n_parcelpoints=self.n_parcelpoints,adjacency=self.adjacency,hgs_time=args.hgs_reopt_time)
-            return env, False, env.action_space.dtype == np.float32
+            return env, False
 
 if __name__ == '__main__':
     pass